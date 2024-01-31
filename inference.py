c9f13abfc774e2dcb350fd7e9980caf065ebca97
Richard Hull <rm_hull@yahoo.co.uk>
2024-01-31 22:51:30 +0000

    Different style & color for detected objects

--- inference.py
+++ inference.py
@@ -45,6 +45,12 @@ class Inferencer:
             width / self._inference_size[0],
             height / self._inference_size[1],
         )
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1.0
+        font_thickness = 2
+        font_color = (255, 255, 255)
+        background_color = (0, 0, 255)
+
         for obj in objs:
             bbox = obj.bbox.scale(scale_x, scale_y)
             x0, y0 = int(bbox.xmin), int(bbox.ymin)
@@ -52,16 +58,27 @@ class Inferencer:
 
             percent = int(100 * obj.score)
             label = "{}% {}".format(percent, self.label(obj.id))
+            (text_width, text_height), baseline = cv2.getTextSize(
+                label, font, font_scale, font_thickness
+            )
 
-            cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
-            cv2_im = cv2.putText(
+            cv2.rectangle(cv2_im, (x0, y0), (x1, y1), background_color, 2)
+            cv2.rectangle(
                 cv2_im,
-                label,
-                (x0, y0 + 30),
-                cv2.FONT_HERSHEY_SIMPLEX,
-                1.0,
-                (255, 0, 0),
-                2,
+                (x0, y0),
+                (x0 + text_width, y0 + text_height),
+                background_color,
+                thickness=cv2.FILLED,
+            )
+
+            cv2.putText(
+                img=cv2_im,
+                text=label,
+                org=(x0, y0 + text_height),
+                fontFace=font,
+                fontScale=font_scale,
+                color=font_color,
+                font_thickness=font_thickness,
             )
         return cv2_im
 
